350d570f5534f298bec6b1a355cd0eade1086177
Antonín Karola <karolan1@fel.cvut.cz>
2016-04-30 14:48:24 +0200

    Basic unauthorized home page layout

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -8,25 +8,38 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('home', {
-            parent: 'app',
-            url: '/',
-            data: {
-                authorities: []
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/home/home.html',
-                    controller: 'HomeController',
-                    controllerAs: 'vm'
+        $stateProvider
+            .state('home', {
+                parent: 'app',
+                url: '/',
+                data: {
+                    authorities: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/home/home.html',
+                        controller: 'HomeController',
+                        controllerAs: 'vm'
+                    },
+                    'login@home': {
+                        templateUrl: 'app/components/login/login-home.html',
+                        // controller: 'LoginController',
+                        // controllerAs: 'coto'
+                    },
+                    'register@home': {
+                        templateUrl: 'app/account/register/register.html',
+                        // controller: 'LoginController',
+                        // controllerAs: 'coto'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('home');
+                        $translatePartialLoader.addPart('login');
+                        $translatePartialLoader.addPart('register');
+                        return $translate.refresh();
+                    }]
                 }
-            },
-            resolve: {
-                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
-                    $translatePartialLoader.addPart('home');
-                    return $translate.refresh();
-                }]
-            }
-        });
+            });
     }
 })();
