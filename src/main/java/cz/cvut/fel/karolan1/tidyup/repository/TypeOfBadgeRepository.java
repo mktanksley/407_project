9e71928520988c4f30333e11ab812caa4bfe9011
Antonín Karola <karolan1@fel.cvut.cz>
2016-05-17 17:47:17 +0200

    Update typeOfBadge

--- src/main/java/cz/cvut/fel/karolan1/tidyup/repository/TypeOfBadgeRepository.java
+++ src/main/java/cz/cvut/fel/karolan1/tidyup/repository/TypeOfBadgeRepository.java
@@ -1,14 +1,12 @@
 package cz.cvut.fel.karolan1.tidyup.repository;
 
 import cz.cvut.fel.karolan1.tidyup.domain.TypeOfBadge;
-
-import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Spring Data JPA repository for the TypeOfBadge entity.
  */
+@SuppressWarnings("unused")
 public interface TypeOfBadgeRepository extends JpaRepository<TypeOfBadge,Long> {
 
 }
